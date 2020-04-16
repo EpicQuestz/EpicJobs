bf953c9d2ae3bfbc4251b7980129c62bd8e8bc0b
StealWonders <6114858+StealWonders@users.noreply.github.com>
2020-04-16 13:07:01 +0200

    Project and job locations now respect where the player is looking (yaw & pitch).

--- src/main/java/de/stealwonders/epicjobs/utils/Utils.java
+++ src/main/java/de/stealwonders/epicjobs/utils/Utils.java
@@ -16,16 +16,16 @@ import java.util.UUID;
 public class Utils {
 
     public static String serializeLocation(@NonNull final Location location) {
-        return location.getWorld().getName() + " " + location.getX() + " " + location.getY() + " " + location.getZ();
+        return location.getWorld().getName() + " " + location.getX() + " " + location.getY() + " " + location.getZ() + " " + location.getYaw() + " " + location.getPitch();
     }
 
     public static Location deserializeLocation(final String input) {
         Location location = null;
         final String[] parts = input.split(" ");
-        if (parts.length >= 4) {
+        if (parts.length >= 6) {
             final World world = Bukkit.getWorld(parts[0]);
             if (world != null) {
-                location = new Location(world, Double.parseDouble(parts[1]), Double.parseDouble(parts[2]), Double.parseDouble(parts[3]));
+                location = new Location(world, Double.parseDouble(parts[1]), Double.parseDouble(parts[2]), Double.parseDouble(parts[3]), Float.parseFloat(parts[4]), Float.parseFloat(parts[5]));
             }
         }
         return location;
