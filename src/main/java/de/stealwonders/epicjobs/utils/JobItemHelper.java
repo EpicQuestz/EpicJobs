e1c53eb064bb66770cb16634b59458a4973fcddd
StealWonders <6114858+StealWonders@users.noreply.github.com>
2020-06-28 00:08:40 +0200

    Added JobItemHelper & MenuHelper

---
+++ src/main/java/de/stealwonders/epicjobs/utils/JobItemHelper.java
@@ -0,0 +1,43 @@
+package de.stealwonders.epicjobs.utils;
+
+import de.stealwonders.epicjobs.job.Job;
+import org.bukkit.ChatColor;
+import org.bukkit.inventory.ItemStack;
+
+public class JobItemHelper {
+
+    public enum InfoType {
+
+        PROJECT, CATEGORY, STATUS, DESCRIPTION, CREATOR, CLAIMANT;
+    }
+
+    public static ItemStack getJobItem(final Job job, final String actionMessage, final InfoType... infoList) {
+        final ItemStackBuilder itemStackBuilder = new ItemStackBuilder(job.getJobCategory().getMaterial())
+            .withName("§f§lJob " + job.getId())
+            .withLore(actionMessage);
+        for (final InfoType informationType : infoList) {
+            switch (informationType) {
+                case PROJECT:
+                    itemStackBuilder.withLore("§f§lProject: §f" + job.getProject().getName());
+                    break;
+                case CATEGORY:
+                    itemStackBuilder.withLore("§f§lCategory: §f" + job.getJobCategory().getName());
+                    break;
+                case STATUS:
+                    itemStackBuilder.withLore("§f§lStatus: §f" + job.getJobStatus().name());
+                    break;
+                case DESCRIPTION:
+                    itemStackBuilder.withLineBreakLore(ChatColor.GRAY, job.getDescription());
+                    break;
+                case CREATOR:
+                    itemStackBuilder.withLore("§f§lCreator: §f" + Utils.getPlayerHolderText(job.getCreator()));
+                    break;
+                case CLAIMANT:
+                    itemStackBuilder.withLore("§f§lClaimant: §f" + Utils.getPlayerHolderText(job.getClaimant()));
+                    break;
+            }
+        }
+
+        return itemStackBuilder.build();
+    }
+}
